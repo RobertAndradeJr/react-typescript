beb2ed32a223eb9ccd9a4c77593a9563c693fb9e
Robert Andrade <srandradejr@gmail.com>
2019-07-20 09:32:37 -0500

    todo deletes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, { Fragment, useState } from 'react';
 // import ReactDOM from 'react-dom';
 import './App.css';
 // import { summarizers } from 'istanbul-lib-report';
@@ -16,28 +16,41 @@ const App: React.FC = () => {
   const [value, setValue] = useState<string>('');
   const [todos, setTodos] = useState<ITodo[]>([]);
 
-  const handleSubmit = (e:FormElem):void => {
+  const handleSubmit = (e: FormElem): void => {
     e.preventDefault();
     addTodo(value);
     setValue('');
   }
 
-
-  const addTodo = (text:string) => {
-    const newTodos: ITodo[] = [...todos, {text, complete: false}];
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
     setTodos(newTodos);
   }
+
+  const completeTodo = (key: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[key].complete = !newTodos[key].complete;
+    setTodos(newTodos);
+  }
+
 
   return (
     <Fragment>
       <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={e => setValue(e.target.value)} required/>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} required />
         <button type="submit">Add todo</button>
       </form>
       <div>
         <ol>
-          {todos.map((todo: ITodo, key: number) => <li key={key}>{todo.text} <u>Complete:</u> {(todo.complete.toString() ? "❌" : "✅")}</li>)}
+          {todos.map((todo: ITodo, key: number) => (
+            <Fragment key={key}>
+              <li><span style={{ textDecoration: todo.complete ? 'line-through' : ''}}>{todo.text}</span> {(todo.complete ? "❌" : "✅")} </li>
+              <button onClick={() => completeTodo(key)}>
+                Complete
+              </button>
+            </Fragment>
+          ))}
         </ol>
       </div>
     </Fragment>
