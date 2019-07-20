3634b4ab3cdd33f6220cec7ae4aad474a85ba6b2
Robert Andrade <srandradejr@gmail.com>
2019-07-20 15:31:09 -0500

    creating redux in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React, { Fragment, useState } from 'react';
-// import ReactDOM from 'react-dom';
+import React, { Fragment, useState, useContext } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-// import { summarizers } from 'istanbul-lib-report';
-// import { number } from 'prop-types';
+import {Store} from './store';
 
 type FormElem = React.FormEvent<HTMLFormElement>
 
@@ -12,57 +11,14 @@ interface ITodo {
 }
 
 const App: React.FC = () => {
-
-  const [value, setValue] = useState<string>('');
-  const [todos, setTodos] = useState<ITodo[]>([]);
-
-  const handleSubmit = (e: FormElem): void => {
-    e.preventDefault();
-    addTodo(value);
-    setValue('');
-  }
-
-  const addTodo = (text: string): void => {
-    const newTodos: ITodo[] = [...todos, { text, complete: false }];
-    setTodos(newTodos);
-  }
-
-  const completeTodo = (key: number): void => {
-    const newTodos: ITodo[] = [...todos];
-    newTodos[key].complete = !newTodos[key].complete;
-    setTodos(newTodos);
-  }
-
-  const deleteTodo = (key: number): void => {
-    const newTodos: ITodo[] = [...todos];
-    newTodos.splice(key, 1);
-    setTodos(newTodos);
-  }
-
+  const store:any = useContext(Store)
 
   return (
-    <Fragment>
-      <h1>Todo List</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={e => setValue(e.target.value)} required />
-        <button type="submit">Add todo</button>
-      </form>
-      <div>
-        <ol>
-          {todos.map((todo: ITodo, key: number) => (
-            <Fragment key={key}>
-              <li><span style={{ textDecoration: todo.complete ? 'line-through' : ''}}>{todo.text}</span> {(todo.complete ? "❌" : "✅")} </li>
-              <button type="button" onClick={() => completeTodo(key)}>
-                Complete
-              </button>
-              <button type="button" onClick={() => deleteTodo(key)}>
-                Delete
-              </button>
-            </Fragment>
-          ))}
-        </ol>
-      </div>
-    </Fragment>
+    <>
+    {console.log(store)};
+      <h1>Avatar</h1>
+      <p>Pick your favorite episode:</p>
+    </>
   );
 }
 
