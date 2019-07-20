9b844ea27f026a3db1857e192b212777833664b6
Robert Andrade <srandradejr@gmail.com>
2019-07-19 20:54:43 -0500

    added todo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment, useState} from 'react';
+// import ReactDOM from 'react-dom';
 import './App.css';
+// import { summarizers } from 'istanbul-lib-report';
+// import { number } from 'prop-types';
+
+type FormElem = React.FormEvent<HTMLFormElement>
+
+interface ITodo {
+  text: string,
+  complete: boolean
+}
 
 const App: React.FC = () => {
+
+  const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e:FormElem):void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+  }
+
+
+  const addTodo = (text:string) => {
+    const newTodos: ITodo[] = [...todos, {text, complete: false}];
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} required/>
+        <button type="submit">Add todo</button>
+      </form>
+      <div>
+        <ol>
+          {todos.map((todo: ITodo, key: number) => <li key={key}>{todo.text} <u>Complete:</u> {(todo.complete.toString() ? "❌" : "✅")}</li>)}
+        </ol>
+      </div>
+    </Fragment>
   );
 }
 
