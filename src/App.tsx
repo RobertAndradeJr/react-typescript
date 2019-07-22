71e3d7031c470303b6bb4d4b7ab8fccfb77cf529
Robert Andrade <srandradejr@gmail.com>
2019-07-21 19:27:51 -0500

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 // import ReactDOM from 'react-dom';
+import "./index.css";
 import "./App.css";
 import { Store } from "./store";
 
@@ -11,18 +12,19 @@ interface ITodo {
 }
 
 const App: React.FC = () => {
-  const {state, dispatch} = React.useContext(Store);
+  const { state, dispatch } = React.useContext(Store);
 
   React.useEffect(() => {
-      state.episodes.length === 0 && fetchDataAction()
-  })
+    state.episodes.length === 0 && fetchDataAction();
+  });
 
   const fetchDataAction = async () => {
-    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=avatar+the+last+airbender&embed=episodes'
+    const URL =
+      "https://api.tvmaze.com/singlesearch/shows?q=avatar+the+last+airbender&embed=episodes";
     const data = await fetch(URL);
     const dataJSON = await data.json();
     return dispatch({
-      type: 'FETCH_DATA',
+      type: "FETCH_DATA",
       payload: dataJSON._embedded.episodes
     });
   };
@@ -31,16 +33,19 @@ const App: React.FC = () => {
       <h1>Avatar</h1>
       <p>Pick your favorite episode:</p>
       <section>
-        {state.episodes.map((episode: any) => 
+        {state.episodes.map((episode: any) => (
           <section key={episode.id}>
-            <img src={episode.image.medium} alt={`Avatar Episode: ${episode.name}`}/>
+            <img
+              src={episode.image.medium}
+              alt={`Avatar Episode: ${episode.name}`}
+            />
             <p>{episode.name}</p>
             <section>
               Season: {episode.season} <br />
               Episode: {episode.number}
             </section>
           </section>
-            )}
+        ))}
       </section>
     </React.Fragment>
   );
